2a9d0adf56f227b157582f41dfb90344eb7d187b
k-umeda <shooting2006513@yahoo.co.jp>
2020-02-02 16:35:08 +0900

    Configure an App

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,20 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    private String wellcomMessage;
+
+    public WelcomeController(@Value("${welcome.message}") String wellcomMessage) {
+        this.wellcomMessage = wellcomMessage;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return wellcomMessage;
     }
 }
